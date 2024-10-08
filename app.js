8630fbb3ef2866474f668bb4972a83284a6fdfe9
cardaropolim <m.cardaropoli@live.it>
2024-10-08 21:40:50 +0200

    final

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
     const express = require('express')
-    const morgan = require('morgan')
     const favicon = require('serve-favicon')
     const bodyParser = require('body-parser')
     const sequelize = require('./src/db/sequelize')
@@ -10,10 +9,13 @@
 
     app
         .use(favicon(__dirname + '/favicon.ico'))
-        .use(morgan('dev'))
         .use(bodyParser.json());
 
     sequelize.initDb()
+
+    app.get('/', (req, res) => {
+        res.json('Hello, Heroku 👋🏻')
+    })
 
     require('./src/routes/findAllPokemons')(app)
     require('./src/routes/findPokemonByPk')(app)
